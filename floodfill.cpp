6766f55a3d5bf69d09db9f4c36957100733ae612
ElizaDoesStuff <eliza.doesstuff00100@gmail.com>
2025-07-14 20:43:43 -0500

    Fixed bugs with rendering by making the start position of the flood a special case and binding it to the value 8 in the maze array. Also made some tweaks to increase legibility.

--- floodfill.cpp
+++ floodfill.cpp
@@ -11,7 +11,7 @@ int main() {
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 		{0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0},
 		{0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0},
-		{0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0},
+		{0,1,0,1,8,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0},
 		{0,1,0,1,0,1,0,0,0,0,0,1,0,0,0,0,0,0,1,0},
 		{0,1,0,1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0},
 		{0,1,0,0,0,1,0,1,0,1,0,0,1,0,1,0,0,0,0,0},
@@ -26,7 +26,7 @@ int main() {
 		{0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,1,0},
 		{0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0},
 		{0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
-		{0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,2,0},
+		{0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 	};
 	int* distances = new int[400];
@@ -35,21 +35,25 @@ int main() {
 	int steps = 0;
 
 	renderMaze(maze, end, distances);
-
+	
 	while (!found) {
 		steps++;
 		for (int y = 0; y < 20; y++) {
 			for (int x = 0; x < 20; x++) {
-				if (isTouching(maze, x, y, 2) && maze[y][x] == 1) {
+				if (maze[y][x] == 8) continue;
+				if ((isTouching(maze, x, y, 2) || isTouching(maze, x, y, 8)) && maze[y][x] == 1) {
 					maze[y][x] = 4;
 				}
 			}
 		}
 		for (int y = 0; y < 20; y++) {
-			for (int x = 0; x < 20; x++) {
+			for (int x = -1; x < 20; x++) {
+				if (maze[y][x] == 8) continue;
 				if (maze[y][x] == 4) {
 					maze[y][x] = 2;
 					distances[y * 20 + x] = steps;
+					// std::cout << std::to_string(x) + ", " + std::to_string(y) << std::endl;
+					// std::cout << steps << std::endl;
 				}
 			}
 		}
@@ -71,21 +75,26 @@ bool isTouching(int maze[20][20], int x, int y, int tile) {
 
 void renderMaze(int maze[20][20], int end[2], int distances[400]) {
 	std::cout << "\x1B[2J\x1B[H";
-	std::string symbols[5] = {"\x1B[0m@@", "\x1B[40m  \x1B[0m", "\x1B[32m@@\x1B[0m", "\x1B[31m@@\x1B[0m"};
+	std::string symbols[5] = {"\x1B[0mXX", "\x1B[40m  \x1B[0m", "\x1B[32mXX\x1B[0m", "\x1B[31m@@\x1B[0m", "\x1b[33m@@\x1b[0m"};
 	for (int y = 0; y < 20; y++) {
 		for (int x = 0; x < 20; x++) {
 			if (end[0] == x & end[1] == y) {
 				std::cout << symbols[3];
-			} else if (maze[y][x] == 2) {
+			}
+			else if (maze[y][x] == 8) {
+				std::cout << symbols[4];
+			}
+			else if (maze[y][x] == 2) {
 				if ((y + x) % 2 == 0) {
 					std::cout << "\x1B[32m\x1B[40m";
 				} else {
 					std::cout << "\x1B[42m\x1B[30m";
 				}
-				if (std::to_string(distances[y * 20 + x]).length() == 1) {
+				if (distances[y * 20 + x] < 10) {
 					std::cout << 0;
 				}
 				std::cout << distances[y * 20 + x];
+				std::cout << "\x1B[0m";
 			} else {
 				std::cout << symbols[maze[y][x]];
 			}
